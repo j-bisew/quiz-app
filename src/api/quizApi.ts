285dbb3fb87371cdb26eb3afbf94ac02d2b7c40d
Julia Bisewska <bisewskaj@gmail.com>
2025-01-16 23:03:26 +0100

    creating database and adding new feature of getting and creating quizzes

---
+++ src/api/quizApi.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import { Request, Response } from "express";
+import prisma from "../db/prisma";
+const router = express.Router();
+
+router.get("/", getQuizzes);
+router.post("/", createQuiz);
+
+async function getQuizzes(_req: Request, res: Response) {
+    try {
+        const quizzes = await prisma.quiz.findMany({
+            include: {
+                questions: true,
+                createdBy: {
+                    select: {
+                        id: true,
+                        name: true,
+                    },
+                }
+            },
+        });
+        res.json(quizzes);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching quizzes' });
+    }
+}
+
+async function createQuiz(req: Request, res: Response) {
+    try {
+        const { title, description, questions, category, difficulty, timeLimit } = req.body;
+        const quiz = await prisma.quiz.create({
+            data: {
+                title,
+                description,
+                category,
+                createdById: 'tmp-user-id',
+                difficulty,
+                timeLimit,
+                questions: {
+                    create: questions,
+                },
+            },
+            include: {
+                questions: true,
+            }
+        });
+        res.status(201).json(quiz);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while creating a quiz' });
+    }
+}
